f5574d16bcb7434cb963cd17918a158b94b7b403
Ozge <erolozge01@gmail.com>
2022-07-07 16:33:04 -0500

    add coffee item to db from UI

--- server.js
+++ server.js
@@ -1,28 +1,92 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const path = require("path");
-const NodeCouchdb = require("node-couchdb");
+const fs = require("fs");
+const https = require("https");
 
-const couch = new NodeCouchdb({
+const express = require("express");
+const app = express();
+
+const bodyParser = require("body-parser");
+const NodeCouchDb = require("node-couchdb");
+const path = require("path");
+
+// Global Vars
+app.use(function (req, res, next) {
+  res.locals.errors = null;
+  next();
+});
+
+//support parsing of application/json type post data
+app.use(bodyParser.json());
+
+//support parsing of application/x-www-form-urlencoded post data
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Connect to CouchDB
+const couchExternal = new NodeCouchDb({
   auth: {
     user: "admin",
     password: "passw0rd",
   },
 });
 
-couch.listDatabases().then(function (dbs) {
-  console.log('databases : ',dbs);
-});
-
-const app = express();
+// View Engine
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.get("/", function (req, res) {
-  //res.send("Working........");
-  res.render('index')
+
+// List all databases to console
+couchExternal.listDatabases().then(function (dbs) {
+  console.log("databases : ", dbs);
 });
+
+//database name
+const dbName = "allcoffee";
+
+//all is the name of the MapReduce views
+const viewUrl2 = "/_all_docs?include_docs=true";
+
+//WEB ROUTES
+
+//INDEX root website/SHOW
+app.get("/", function (req, res) {
+  couchExternal.get(dbName, viewUrl2).then(
+    function (data, headers, status) {
+      res.render("index", {
+        allcoffee: data.data.rows,
+      });
+    },
+    function (err) {
+      res.send(err);
+    }
+  );
+});
+
+//STORE Coffee
+app.post("/coffee/add", function (req, res) {
+  couchExternal.get(dbName, viewUrl2).then(
+    function (data, headers, status) {
+      res.render("index", {
+        allcoffee: data.data.rows,
+      });
+    },
+    function (err) {
+      res.send(err);
+    }
+  );
+
+  let newCoffee = {
+    name: req.body.name,
+    coffee_type: req.body.coffee_type,
+    flavor: req.body.flavor,
+  };
+  couchExternal.insert(dbName, newCoffee).then(
+    function (data, headers, status) {
+      res.redirect("/");
+    },
+    function (err) {
+      res.send(err);
+    }
+  );
+});
+
 app.listen(3000, function () {
   console.log("Server is started on Port 3000");
 });
